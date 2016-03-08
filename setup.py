1694a8bd71a258f5bc4dbdcd5ab9c1cda6a6b57d
athre0z <athre0z@mainframe.pw>
2016-03-08 19:47:21 +0100

    initial commit of actual code

---
+++ setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf8 -*-
+from distutils.core import setup
+
+setup(
+    name='twocaptchaapi',
+    version='0.1',
+    packages=['twocaptchaapi'],
+    url='https://github.com/athre0z/twocaptcha-api',
+    license='MIT',
+    author='Joel Höner (athre0z)',
+    author_email='joel@zyantific.com',
+    description='Python API implementation for 2Captcha.com'
+)
